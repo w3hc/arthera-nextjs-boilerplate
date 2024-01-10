42a3e1a68c5b1613271cce59fb1e9b53023b9ee9
Julien Béranger <julien@strat.cc>
2024-01-10 15:30:37 +0100

    Switch to Sepolia (#4)

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 import { ThemingProps } from '@chakra-ui/react'
-import { Chain } from '@wagmi/core'
+import { Chain, sepolia } from '@wagmi/core'
 
 export const SITE_NAME = 'W3HC Next.js Boilerplate'
 export const SITE_DESCRIPTION = 'A variant of Nexth boilerplate for hackathons and quick prototyping.' // Your description should be between 55 and 200 characters long, with a maximum of 300.
@@ -14,26 +14,7 @@ export const THEME_CONFIG = {
 export const SOCIAL_TWITTER = 'w3hc8'
 export const SOCIAL_GITHUB = 'w3hc/w3hc-nextjs-boilerplate'
 
-export const artheraTestnet: Chain = {
-  id: 10243,
-  name: 'Arthera Testnet',
-  network: 'artheraTestnet',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'AA',
-    symbol: 'AA',
-  },
-  rpcUrls: {
-    public: { http: ['https://rpc-test.arthera.net'] },
-    default: { http: ['https://rpc-test.arthera.net'] },
-  },
-  blockExplorers: {
-    etherscan: { name: 'Arthera Testnet Explorer', url: 'https://explorer-test.arthera.net' },
-    default: { name: 'Arthera Testnet Explorer', url: 'https://explorer-test.arthera.net' },
-  },
-} as const satisfies Chain
-
-export const ETH_CHAINS = [artheraTestnet]
+export const ETH_CHAINS = [sepolia]
 
 export const SERVER_SESSION_SETTINGS = {
   cookieName: SITE_NAME,
